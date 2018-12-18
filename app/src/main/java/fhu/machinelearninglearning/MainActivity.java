1afa25c3b5860ad414b814d15d7a59c7602b4190
A.S.Laptop <alshen@unm.edu>
2018-12-18 09:53:22 -0700

    initialized main menu activity

--- app/src/main/java/fhu/machinelearninglearning/MainActivity.java
+++ app/src/main/java/fhu/machinelearninglearning/MainActivity.java
@@ -1,7 +1,10 @@
 package fhu.machinelearninglearning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +15,16 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //sup
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
